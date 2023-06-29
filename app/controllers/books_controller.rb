37ea5ef842f955b485c1657e2008445bac95ed42
kikuchikk <129989605+kikuchikk@users.noreply.github.com>
2023-06-29 12:39:50 +0000

    [Update]3-13-18

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,16 +7,25 @@ class BooksController < ApplicationController
 def create
   book = Book.new(book_params)
   book.save
-  redirect_to '/'
+  redirect_to book_path(book.id)
 end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
